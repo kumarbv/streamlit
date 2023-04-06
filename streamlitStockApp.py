07d3ea8b56d0e4500da3387a9d38b379ed1ed584
Kumar B V <kumar.bv@gmail.com>
2023-04-06 13:10:26 +0530

    Update streamlitStockApp.py

--- streamlitStockApp.py
+++ streamlitStockApp.py
@@ -5,3 +5,9 @@ import yfinance as yf
 st.write("""
 Simple Stock Price App
 """)
+
+ticker = 'GOOG'
+tickerData = yf.Ticker(ticker)
+tickerDf = tickerData.history(period='1d', start='2023-01-01', end='2023-04-05')
+st.line_chart(tickerDf.close)
+st.line_chart(tickerDf.Volume)
