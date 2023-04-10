bb6b9503a7abcd777955949ba37cd7fe8d68f38e
Kumar B V <kumar.bv@gmail.com>
2023-04-10 20:00:57 +0530

    Update streamlitStockApp.py

--- streamlitStockApp.py
+++ streamlitStockApp.py
@@ -36,7 +36,7 @@ sorted_returns = overall_returns.sort_values(ascending=False)
 data = {"Stocks":sorted_returns.index.to_numpy(), "Values": sorted_returns.values*100}
 data = pd.DataFrame(data)
 st.dataframe(data)
-data = data.set_index()
+data = data.set_index("Stocks")
 st.bar_chart(data)
 
 # Print the overall returns for each stock
